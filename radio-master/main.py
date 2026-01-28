e375df31601db14c01c0da0746d49c1356f20bd7
Jan Tacakiewicz <jan.tacakiewicz@protonmail.com>
2026-01-28 17:40:36 +0100

    main: tested logic surrounding an effect (using dummy for now)

--- radio-master/main.py
+++ radio-master/main.py
@@ -1,10 +1,15 @@
 from players.spotify_player import SpotifyPlayer
 from controller.keyboard_controller import KeyboardController
-from buttons import RequestButtons
+from mixer.mixer import Mixer
+from mixer.dummy import DummyEffect
+from buttons import EffectButtons
 
 sp = SpotifyPlayer()
 kc = KeyboardController()
+mx = Mixer(use_pyo=False)
+mx.addEffect(DummyEffect, effect_type=EffectButtons.Jazz)
+
 kc.setVolumeCallback(lambda x, y: print(f"Volume: {y}", end="\r\n"))
 kc.setRequestCallback(lambda req: sp.switch(req))
-kc.setEffectCallback(lambda e, a: print(f"Effect: {e}-{a}", end="\r\n"))
+kc.setEffectCallback(lambda e, active: mx.on(e) if active else mx.off(e))
 kc.run_loop()
